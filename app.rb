44f9de798d2b2ac6695bf22c13b2270e11198d55
said shah <saidmysr20@aol.com>
2018-10-29 11:02:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Said"
+  end
+
+  get "/hometown" do
+    "My hometown is Kabul"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Music"
+  end
+
+
 end
